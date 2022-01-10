04284864118812c844282f394bf3188f2d07f787
Yerin Park <1472moment@gmail.com>
2022-01-10 18:10:55 +0900

    BlogForm 생성

--- src/components/blog/BlogForm.jsx
+++ src/components/blog/BlogForm.jsx
@@ -1,7 +1,38 @@
-function BlogForm() {
+function BlogForm({ fieldValues, handleFieldChange, handleSubmit, loading }) {
+  const handleClickedSubmitButton = () => {
+    if (handleSubmit) {
+      handleSubmit();
+    } else {
+      console.warn('handleSubmit 속성값을 지정해주세요.');
+    }
+  };
+
   return (
     <div>
-      <h4>form</h4>
+      {loading && 'Loading...'}
+      <label>Title</label>
+      <text
+        onChange={handleFieldChange}
+        name="title"
+        value={fieldValues.title}
+        className="bg-gray-200"
+      />
+
+      <label>Content</label>
+      <textarea
+        onChange={handleFieldChange}
+        name="content"
+        value={fieldValues.content}
+        className="border-gray-300"
+      />
+
+      <button
+        type="submit"
+        onClick={() => handleClickedSubmitButton()}
+        disabled={loading}
+      >
+        {loading && 'loading...'} 저장하기
+      </button>
     </div>
   );
 }
